9a760dd2bcfb863a3a23e58f72ed7e4191043dbc
Hareki <minhtu1392000@gmail.com>
2023-03-02 12:18:08 +0700

    config: re-config theme color and shadow

--- src/theme/MuiTheme.tsx
+++ src/theme/MuiTheme.tsx
@@ -29,10 +29,18 @@ const MuiTheme: FC<MuiThemeProps> = ({ children }) => {
   theme = responsiveFontSizes(theme);
 
   // theme shadows
-  theme.shadows[1] = '0px 1px 3px rgba(3, 0, 71, 0.09)';
-  theme.shadows[2] = '0px 4px 16px rgba(43, 52, 69, 0.1)';
-  theme.shadows[3] = '0px 8px 45px rgba(3, 0, 71, 0.09)';
-  theme.shadows[4] = '0px 0px 28px rgba(3, 0, 71, 0.01)';
+  // theme.shadows[1] = '0px 1px 3px rgba(3, 0, 71, 0.09)';
+  // theme.shadows[2] = '0px 4px 16px rgba(43, 52, 69, 0.1)';
+  // theme.shadows[3] = '0px 8px 45px rgba(3, 0, 71, 0.09)';
+  // theme.shadows[4] = '0px 0px 28px rgba(3, 0, 71, 0.01)';
+
+  theme.shadows[1] =
+    '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)';
+  theme.shadows[2] =
+    '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)';
+  theme.shadows[3] =
+    '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)';
+  theme.shadows[4] = '0 25px 50px -12px rgb(0 0 0 / 0.25)';
 
   return (
     <ThemeProvider theme={theme}>
--- src/theme/themeOptions.ts
+++ src/theme/themeOptions.ts
@@ -1,15 +1,12 @@
 import { ThemeOptions } from '@mui/material/styles';
 
 import { components } from './components';
-import { blue, marron, paste, primary, themeColors } from './themeColors';
+import { primary, themeColors } from './themeColors';
 import { typography } from './typography';
 
 const THEMES = {
-  GIFT: 'GIFT',
-  HEALTH: 'HEALTH',
   DEFAULT: 'DEFAULT',
-  GROCERY: 'GROCERY',
-  FURNITURE: 'FURNITURE',
+  HEALTH: 'HEALTH',
 };
 
 const breakpoints = {
@@ -22,11 +19,6 @@ const breakpoints = {
   },
 };
 
-/*
-WE CREATED MULTIPLE THEME OPTIONS FOR DIFFERENT SHOP VARIATION.
-
-YOU CAN JUST KEEP [THEMES.DEFAULT] AND REMOVE OTHER THEME OPTIONS.
-*/
 const themesOptions: ThemeOptions = {
   [THEMES.DEFAULT]: {
     typography,
@@ -34,76 +26,11 @@ const themesOptions: ThemeOptions = {
     components: { ...components },
     palette: { primary: { ...primary, light: primary[100] }, ...themeColors },
   },
-  [THEMES.GROCERY]: {
-    typography,
-    breakpoints,
-    components: { ...components },
-    palette: { primary: { ...primary, light: primary[100] }, ...themeColors },
-  },
-  [THEMES.FURNITURE]: {
-    typography,
-    breakpoints,
-    components: { ...components },
-    palette: { primary: { ...paste, light: paste[100] }, ...themeColors },
-  },
-  [THEMES.HEALTH]: {
-    typography,
-    breakpoints,
-    components: { ...components },
-    palette: { primary: { ...blue, light: blue[100] }, ...themeColors },
-  },
-  [THEMES.GIFT]: {
-    typography,
-    breakpoints,
-    components: { ...components },
-    palette: { primary: { ...marron, light: marron[100] }, ...themeColors },
-  },
 };
 
-const themeOptions = (publicRuntimeConfig: any, pathname: string) => {
-  let themeOptions: ThemeOptions;
-
-  /*
-    YOU CAN ALSO REMOVE updateTheme function
-    AND FOLLOWING ENTIRE switch case BLOCK.
-  */
-  const updateTheme = (themeName: string) => {
-    publicRuntimeConfig.theme = themeName;
-    themeOptions = themesOptions[publicRuntimeConfig.theme];
-  };
-
-  switch (pathname) {
-    case '/':
-    case '/grocery1':
-    case '/grocery2':
-    case '/grocery3':
-    case '/gadget-shop':
-    case '/fashion-shop-1':
-    case '/market-1':
-      updateTheme(THEMES.DEFAULT);
-      break;
-
-    case '/furniture-shop':
-      updateTheme(THEMES.FURNITURE);
-      break;
-
-    case '/healthbeauty-shop':
-      updateTheme(THEMES.HEALTH);
-      break;
-
-    case '/gift-shop':
-      updateTheme(THEMES.GIFT);
-      break;
-
-    default:
-      themeOptions = themesOptions[publicRuntimeConfig.theme];
-      break;
-  }
-  /*
-        IF YOU REMOVE THE switch case, YOU NEED TO ASSIGN VALUE TO themeOptions
-        E.G. themeOptions = themesOptions[THEMES.DEFAULT];
-    */
-  // themeOptions = themesOptions[THEMES.DEFAULT];
+const themeOptions = (publicRuntimeConfig?: any, pathname?: string) => {
+  // publicRuntimeConfig.theme = themesOptions[THEMES.DEFAULT];
+  const themeOptions: ThemeOptions = themesOptions[THEMES.DEFAULT];
 
   return themeOptions;
 };
