e5a28c029c4d4390b3b503d0199f1e246f7a93a2
Hareki <minhtu1392000@gmail.com>
2023-03-20 14:32:17 +0700

    enhance: save stable

--- pages/api/product/all.ts
+++ pages/api/product/all.ts
@@ -1,46 +1,72 @@
 import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
 
-import { getAllProducts } from 'api/base/pre-render';
+import { defaultOnError, defaultOnNoMatch } from 'api/base/next-connect';
+import ProductController from 'api/controllers/Product.controller';
 import connectToDB from 'api/database/databaseConnection';
+import { getPaginationParams } from 'api/helpers/pagination.helpers';
 import type { IProduct } from 'api/models/Product.model/types';
 import type {
   GetPaginationPrerenderResult,
   GetPaginationResult,
 } from 'api/types/pagination.type';
+import type { JSendResponse } from 'api/types/response.type';
 
 type UnionPagination =
   | GetPaginationResult<IProduct>
   | GetPaginationPrerenderResult<IProduct>;
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  switch (req.method) {
-    case 'GET': {
-      console.log('new handler');
-      await connectToDB();
+const handler = nc<
+  NextApiRequest,
+  NextApiResponse<JSendResponse<UnionPagination>>
+>({
+  onError: defaultOnError,
+  onNoMatch: defaultOnNoMatch,
+}).get(async (req, res) => {
+  await connectToDB();
 
-      const { populate, docsPerPage, page } = req.query;
-      const isPopulate = populate === 'true';
+  const { populate, docsPerPage, page } = req.query;
+  const isPopulate = populate === 'true';
 
-      const result = await getAllProducts(
-        docsPerPage as string,
-        page as string,
-        isPopulate,
-      );
+  const result = await getAllProducts(
+    docsPerPage as string,
+    page as string,
+    isPopulate,
+  );
 
-      res.status(StatusCodes.OK).json({
-        status: 'success',
-        data: result,
-      });
-      break;
-    }
-
-    default: {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ errorMessage: 'Method not supported' });
-    }
-  }
-}
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    data: result,
+  });
+});
 
 export default handler;
+
+export async function getAllProducts(
+  docsPerPage: string,
+  page: string,
+  isPopulate: boolean,
+) {
+  const totalDocs = await ProductController.getTotal();
+
+  const { skip, limit, nextPageNum } = getPaginationParams({
+    docsPerPage: docsPerPage,
+    page: page,
+    totalDocs,
+  });
+
+  const products = await ProductController.getAll({
+    populate: isPopulate ? ['category'] : undefined,
+    skip,
+    limit,
+  });
+
+  const result = {
+    nextPageNum,
+    totalDocs,
+    docs: products,
+  };
+  return result;
+}
+
