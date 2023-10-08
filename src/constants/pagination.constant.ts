28aa40be4d350a1ceef954f5e6d7d189896242bf
Nguyen Ngoc Minh Tu <minhtu1392000@gmail.com>
2023-10-08 10:25:17 +0700

    enhance: add constants

---
+++ src/constants/pagination.constant.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_PAGE = 1;
+
+export const DEFAULT_LIMIT = 10;
+
+export const SUBJECT_LIMIT = 10;
+export const QUESTION_LIMIT = 10;
+export const TEST_LIMIT = 10;
+export const LECTURER_LIMIT = 10;
