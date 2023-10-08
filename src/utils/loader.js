587579c2a3bbe86020e59df01fe21787ee1ddc6d
Nguyen Ngoc Minh Tu <minhtu1392000@gmail.com>
2023-10-08 10:40:51 +0700

    migration: remove some redundant files

--- src/utils/loader.js
+++
@@ -1,5 +0,0 @@
-import { lazy } from 'react';
-// export const MyContext = dynamic(() => import("../contexts/ThemeContext"), {
-//   suspense: true,
-// });
-export const MyContext = lazy(() => import('../contexts/ThemeContext'));
