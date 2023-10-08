4308eefeba3910544f0af70b53f7f767f489ae6b
Nguyen Ngoc Minh Tu <minhtu1392000@gmail.com>
2023-10-08 08:33:53 +0700

    enhance: update configs

--- next.config.js
+++ next.config.js
@@ -33,4 +33,26 @@ module.exports = {
       },
     ];
   },
+  /**
+   * @param {import('webpack').Configuration} config
+   * @returns {import('webpack').Configuration}
+   */
+  webpack: (config) => {
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      // entries['main.js'] is the starting point of the project
+      if (
+        entries['main.js'] &&
+        !entries['main.js'].includes('./src/polyfills.ts')
+      ) {
+        entries['main.js'].unshift('./src/polyfills.ts');
+      }
+
+      return entries;
+    };
+
+    return config;
+  },
 };
--- src/components/common/layout/Topbar.tsx
+++ src/components/common/layout/Topbar.tsx
@@ -30,25 +30,27 @@ import { LayoutConstant } from 'utils/constants';
 // styled component
 const TopbarWrapper = styled(Box, {
   shouldForwardProp: (props) => props !== 'bgColor',
-})<{ bgColor: string; expand: number }>(({ theme, bgColor, expand }) => ({
-  fontSize: 12,
-  height: LayoutConstant.topbarHeight,
-  color: theme.palette.secondary.contrastText,
-  background: bgColor || theme.palette.grey[900],
-  transition: 'height 300ms ease',
-  '& .menuItem': { minWidth: 100 },
-  '& .marginRight': { marginRight: '1.25rem' },
-  '& .expand': { display: 'none', padding: 0 },
-  '& .handler': { height: LayoutConstant.topbarHeight },
-  '& .menuTitle': { fontSize: 12, marginLeft: '0.5rem', fontWeight: 600 },
+})<{ bgColor: string | undefined; expand: number }>(
+  ({ theme, bgColor, expand }) => ({
+    fontSize: 12,
+    height: LayoutConstant.topbarHeight,
+    color: theme.palette.secondary.contrastText,
+    background: bgColor || theme.palette.grey[900],
+    transition: 'height 300ms ease',
+    '& .menuItem': { minWidth: 100 },
+    '& .marginRight': { marginRight: '1.25rem' },
+    '& .expand': { display: 'none', padding: 0 },
+    '& .handler': { height: LayoutConstant.topbarHeight },
+    '& .menuTitle': { fontSize: 12, marginLeft: '0.5rem', fontWeight: 600 },
 
-  [theme.breakpoints.down('sm')]: {
-    height: expand ? 80 : LayoutConstant.topbarHeight,
-    '& .topbarRight': { display: expand ? 'flex' : 'none', paddingBottom: 5 },
-    '& .expand': { display: 'block', height: LayoutConstant.topbarHeight },
-    '& .MuiSvgIcon-root': { color: 'white' },
-  },
-}));
+    [theme.breakpoints.down('sm')]: {
+      height: expand ? 80 : LayoutConstant.topbarHeight,
+      '& .topbarRight': { display: expand ? 'flex' : 'none', paddingBottom: 5 },
+      '& .expand': { display: 'block', height: LayoutConstant.topbarHeight },
+      '& .MuiSvgIcon-root': { color: 'white' },
+    },
+  }),
+);
 
 const StyledContainer = styled(Container)(({ theme }) => ({
   height: '100%',
