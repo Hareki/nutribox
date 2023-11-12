cb0be274a0232e10b240f154afaf343bc0c02dd5
Nguyen Ngoc Minh Tu <minhtu1392000@gmail.com>
2023-11-12 21:09:40 +0700

    update OpenGraphTags

--- src/components/abstract/OpenGraphTags.tsx
+++ src/components/abstract/OpenGraphTags.tsx
@@ -3,16 +3,13 @@ import React from 'react';
 const OpenGraphTags: React.FC = () => {
   return (
     <React.Fragment>
-      <meta
-        property='og:url'
-        content='https://bazaar-react.vercel.app/landing'
-      />
+      <meta property='og:url' content='https://nutribox.vercel.app/' />
       {/* thumbnail And title for social media */}
       <meta property='og:type' content='website' />
-      <meta property='og:title' content='Bazaar - React Ecommerce Template' />
+      <meta property='og:title' content='Nutribox' />
       <meta
         property='og:description'
-        content='React Next.js ecommerce template. Build SEO friendly Online store, delivery app and Multi vendor store'
+        content='Website mua bán thực phẩm tươi sống và nhu yếu phẩm'
       />
       <meta property='og:image' content='/assets/images/landing/preview.png' />
     </React.Fragment>
